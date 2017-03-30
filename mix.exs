5ca6414fa3060314352bf0b828992a5afc71a19c
david <davidqhr@gmail.com>
2017-03-30 17:21:18 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Alipay.Mixfile do
 
   def project do
     [app: :alipay,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
